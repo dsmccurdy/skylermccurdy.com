f5ea0fafb0b67519ed75047b9f1b1ccf223a9702
Skyler McCurdy <mccurdyskyler@gmail.com>
2015-05-22 16:32:32 -0600

    Fixed syntax error

--- js/site/controllers/portfolioController.js
+++ js/site/controllers/portfolioController.js
@@ -2,9 +2,9 @@ smcom.controller('PortfolioController',['$scope',function($scope){
 
 	$scope.contact={
 		
-		"email":btoa("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t");
-		"phone":btoa("KDQzNSk2MzMtMDE2Mg==");
+		"email":btoa("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t")
+		"phone":btoa("KDQzNSk2MzMtMDE2Mg==")
 	
-	}
+	};
 
 }]);
