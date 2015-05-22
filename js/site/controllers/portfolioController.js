f9d020e1d8db5c0cf4ddff26fd5b8770ae9c66dc
Skyler McCurdy <mccurdyskyler@gmail.com>
2015-05-22 16:37:29 -0600

    It would help if I put it in the right controller

--- js/site/controllers/portfolioController.js
+++ js/site/controllers/portfolioController.js
@@ -2,8 +2,8 @@ smcom.controller('PortfolioController',['$scope',function($scope){
 
 	$scope.contact={
 		
-		"email":btoa("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t"),
-		"phone":btoa("KDQzNSk2MzMtMDE2Mg==")
+		"email":atob("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t"),
+		"phone":atob("KDQzNSk2MzMtMDE2Mg==")
 	
 	};
 
--- js/site/controllers/resumeController.js
+++ js/site/controllers/resumeController.js
@@ -1,4 +1,10 @@
 smcom.controller('ResumeController',['$scope',function($scope){
 
+	$scope.contact={
+		
+		"email":atob("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t"),
+		"phone":atob("KDQzNSk2MzMtMDE2Mg==")
+	
+	};
 
 }]);
