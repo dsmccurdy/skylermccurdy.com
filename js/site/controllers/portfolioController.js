62367db912a8a99b449444804378f797531517ba
Skyler McCurdy <mccurdyskyler@gmail.com>
2015-05-22 16:33:28 -0600

    More syntax errors =/

--- js/site/controllers/portfolioController.js
+++ js/site/controllers/portfolioController.js
@@ -2,7 +2,7 @@ smcom.controller('PortfolioController',['$scope',function($scope){
 
 	$scope.contact={
 		
-		"email":btoa("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t")
+		"email":btoa("c2t5bGVyQHNreWxlcm1jY3VyZHkuY29t"),
 		"phone":btoa("KDQzNSk2MzMtMDE2Mg==")
 	
 	};
